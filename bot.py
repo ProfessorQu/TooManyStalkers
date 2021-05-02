4b23610700d7f7f14c5c71e2564c59f4c526176c
ProfessorQu <quint@dasookwat.com>
2021-05-02 15:42:21 +0200

    Build pylons, added expansion
    DONE:
    Added self.build_pylons() to build pylons,
    Added self.expand to expand,
    
    TODO:
    Have the bot stop creating workers at a certain point,
    Have the bot train an army,
    And a few other things,

--- bot.py
+++ bot.py
@@ -7,10 +7,27 @@ class QBot(sc2.BotAI):
     async def on_step(self, iteration: int):
         await self.distribute_workers()
         await self.build_workers()
+        await self.build_pylons()
+        await self.expand()
     
     async def build_workers(self):
-        for nexus in self.townhalls:
-            if self.can_afford(UnitTypeId.PROBE):
-                nexus.train(UnitTypeId.PROBE)
-            else:
-                break
+        for nexus in self.townhalls.ready:
+            if nexus.is_idle:
+                if self.can_afford(UnitTypeId.PROBE):
+                    nexus.train(UnitTypeId.PROBE)
+                else:
+                    break
+    
+    async def build_pylons(self):
+        if self.supply_left < 5 and not self.already_pending(UnitTypeId.PYLON):
+            nexuses = self.townhalls.ready
+
+            if nexuses.exists:
+                if self.can_afford(UnitTypeId.PYLON):
+                    await self.build(UnitTypeId.PYLON, near=nexuses.first)
+    
+    async def expand(self):
+        print(self.units(UnitTypeId.NEXUS))
+        
+        if self.townhalls.amount < 2 and self.can_afford(UnitTypeId.NEXUS):
+            await self.expand_now()
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 from bot import QBot
 
-
-run_game(maps.get("Abyssal Reef LE"), [
-    Bot(Race.Protoss, QBot()),
-    Computer(Race.Zerg, Difficulty.Medium)
-], realtime=True)
+try:
+    run_game(maps.get("Abyssal Reef LE"), [
+        Bot(Race.Protoss, QBot()),
+        Computer(Race.Zerg, Difficulty.Medium)
+    ], realtime=True)
+except ValueError:
+    print(f"Game ended.")
