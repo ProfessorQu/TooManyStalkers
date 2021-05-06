f246c6b76f330c86b507d06052bb18e7895db1bb
ProfessorQu <quint@dasookwat.com>
2021-05-06 12:39:27 +0200

    Revert "Moved everything to TooManyStalkers"
    This reverts commit 59a142aa309215e03e7e9d9f736c6dcd627b6266.

---
+++ main.py
@@ -0,0 +1,12 @@
+from sc2 import run_game, maps, Race, Difficulty
+from sc2.player import Bot, Computer
+from bot import QBot
+
+
+try:
+    run_game(maps.get("Abyssal Reef LE"), [
+        Bot(Race.Protoss, QBot()),
+        Computer(Race.Zerg, Difficulty.Medium)
+    ])
+except (ValueError, TypeError):
+    print("Game ended.")
